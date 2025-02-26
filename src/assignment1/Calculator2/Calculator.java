a0447593d7e427c99e8ac067e7eaf7166ec903ab
RKS-t <rudtjs5719@gmail.com>
2025-02-26 19:22:34 +0900

    2번때 계산기 과제 완료

--- src/assignment1/Calculator2/Calculator.java
+++ src/assignment1/Calculator2/Calculator.java
@@ -6,13 +6,13 @@ import java.util.List;
 public class Calculator {
 
 
-    //계산 과정을 저장하는 컬렉션 추가
+    //계산 과정을 저장하는 객체 컬렉션 추가
     private ArrayList<Calculator> calData = new ArrayList<>();
 
     //속성 변수 선언
     public int num1;
     public int num2;
-    public int sign;
+    public char sign;
     private int[] result;
 
     //생성자
@@ -38,6 +38,7 @@ public class Calculator {
         }
 
         //연산 후 그 값을 저장
+        this.result = result;
         calData.add(new Calculator(num1, num2, sign, result));
         return result;
     }
@@ -50,8 +51,12 @@ public class Calculator {
         return result;
     }
 
-    public void setData(List<Calculator> calData){
-        this.calData=new ArrayList<>(calData);
+    public void setData() throws IndexOutOfBoundsException{
+
+            calData.remove(0);
+            this.calData=new ArrayList<>(calData);
+            System.out.println("가장 오래된 계산 정보가 삭제되었습니다.");
+
     }
 
 
--- src/assignment1/Calculator2/Main.java
+++ src/assignment1/Calculator2/Main.java
@@ -16,16 +16,16 @@ public class Main {
 
                 System.out.print("첫번째 양의 정수(0포함)를 입력해주세요: ");
                 int num1 = value.nextInt();
-                String blank = value.nextLine(); //줄바꿈제거
+                value.nextLine(); //줄바꿈제거
 
                 System.out.print("두번째 양의 정수(0포함)를 입력해주세요: ");
                 int num2 = value.nextInt();
-                blank = value.nextLine(); //줄바꿈제거
+                value.nextLine(); //줄바꿈제거
 
                 //사직연산 기호를 입력받기
                 System.out.print("사직연산 기호(+,-,*,/)중 하나를 입력해 주세요: ");
                 char sign = value.next().charAt(0);
-                blank = value.nextLine(); //줄바꿈제거
+                value.nextLine(); //줄바꿈제거
 
 
                 if (sign == '/' && num2 == 0) {
@@ -38,27 +38,68 @@ public class Main {
                         System.out.println(num1 + " " + sign + " " + num2 + " = " + result[0] + "입니다.");
                     } else {
                         System.out.println("잘못된 문자입니다.");
-                        blank = value.nextLine();
+                        value.nextLine();
                     }
                 }
             } catch (InputMismatchException e) {
                 System.out.println("올바른 숫자가 아닙니다."); //양의 정수가 아닌 문자가 입력되었을 때 예외 처리
+                value.nextLine();
             }
-            List<Calculator> history = cal.getData();
-            int[] result = cal.getResult();
 
-            //향상된 for문 사용하여 history 읽기
-            for (Calculator data : history) {
-                System.out.println(data.num1 + " " + (""+data.sign) + " " + data.num2 + "=" + result[0]);
-            }
-            System.out.print("계산기 종료를 원하시면 'exit'를 아니면 아무키나 입력해주세요.: ");
+            System.out.print("계산기 종료를 원하시면 'exit'를 아니면 아무키나 입력해주세요. 다른키를 누르면 종료됩니다.: ");
             String exit = value.nextLine();
             if(exit.equals("exit")){
                 //계산기를 종료할 때 세터로 calData의 값을 전부 초기화 해주기
                 break;
             }
-        }
 
+        }
+        System.out.print("계산 내역을 출력하시겠습니까? 출력을 원하시면 y를 누르세요: ");
+        String historyYN = value.nextLine();
+        if(historyYN.equals("y")){
+            List<Calculator> history = cal.getData();
+            int[] result = cal.getResult();
+            //향상된 for문 사용하여 history 읽기
+            for (Calculator data : history) {
+                if(data.sign == '/'){
+                    System.out.println(data.num1 + " " + data.sign + " " + data.num2 + " = " + result[0] + " 나머지: " + result[1]);
+                }else{
+                    System.out.println(data.num1 + " " + data.sign + " " + data.num2 + " = " + result[0]);
+                }
+            }
+            //삭제 반복문
+
+            while(true) {
+                System.out.print("가장 오래된 계산 내역을 삭제하시겠습니까? 삭제하고 싶으시면 'y' 를 눌러주세요. 'n'을 누르면 종료됩니다.");
+                String removeYN = value.nextLine();
+
+                try {
+                    if (removeYN.equals("y")) {
+                        //세터로 가장 오래된 정보 삭제
+
+                        cal.setData();
+
+                        //삭제 후 데이터 불러오기(꼭 해야되는 것인가?)
+                        List<Calculator> reHistory = cal.getData();
+                        int[] reResult = cal.getResult();
+                        //삭제 후 히스토리 읽기
+                        for (Calculator data : reHistory) {
+                            if (data.sign == '/') {
+                                System.out.println(data.num1 + " " + data.sign + " " + data.num2 + " = " + reResult[0] + " 나머지: " + reResult[1]);
+                            } else {
+                                System.out.println(data.num1 + " " + data.sign + " " + data.num2 + " = " + reResult[0]);
+                            }
+                        }
+                    } else if (removeYN.equals("n")) {
+                        break;
+                    }
+                } catch (IndexOutOfBoundsException n) {
+                    System.out.println("더 이상 삭제 할 수 없습니다. 계산기를 종료합니다.");
+                    break;
+                }
+
+            }
+        }
 
     }
 }
